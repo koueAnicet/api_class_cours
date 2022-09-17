ce3eb3355788183c7a2adb705adb0efb620d3d68
koueAnicet <anicetkoue@gmail.com>
2022-09-17 09:34:52 +0000

    Add disable action for product endpoint

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 
 
 class Category(models.Model):
@@ -9,10 +9,20 @@ class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     active = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.name
 
+    @transaction.atomic
+    def disable(self):
+        if self.active is False:
+        # Ne faisons rien si la catégorie est déjà désactivée
+            return
+        self.active = False
+        self.save()
+        self.products.update(active=False)
+    
 
 class Product(models.Model):
 
@@ -27,6 +37,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    @transaction.atomic
+    def disable(self):
+        if self.active is False:
+            return
+        self.active = False
+        self.save()
+        self.articles.update(active=False)
 
 
 class Article(models.Model):
